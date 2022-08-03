ebd16ba041e9fd10269cb40cd35b84926d0cac55
John Carlo Austria <johncarloaustria@protonmail.com>
2022-08-03 20:24:54 +0800

    Add https protocol
    Signed-off-by: John Carlo Austria <johncarloaustria@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "jaycedotbin.github.io",
+  site: "https://jaycedotbin.github.io",
   integrations: [image(), svelte()],
 });
