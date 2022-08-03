822261f86c1c0c8d105bb9b9ac5a76e3dec7c943
John Carlo Austria <johncarloaustria@protonmail.com>
2022-08-03 20:22:51 +0800

    Add site property
    Signed-off-by: John Carlo Austria <johncarloaustria@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), svelte()]
+  site: "jaycedotbin.github.io",
+  integrations: [image(), svelte()],
 });
