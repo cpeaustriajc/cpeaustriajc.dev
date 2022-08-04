5acd83c215a33c6a6ac3853f57c9d2ddc8610019
John Carlo Austria <johncarloaustria@protonmail.com>
2022-08-04 23:41:42 +0800

    Fixes and additions
    Signed-off-by: John Carlo Austria <johncarloaustria@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,29 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), tailwind(), sitemap()],
-  site: "https://jaycedotbin.github.io",
-  vite: {
-    ssr: {
-      noExternal: ["@fontsource/inter"]
-    }
-  }
+	integrations: [
+		image(),
+		tailwind(),
+		sitemap({
+			i18n: {
+				defaultLocale: "en",
+				locales: {
+					en: "en-US",
+				},
+			},
+		}),
+		mdx(),
+	],
+	site: "https://jaycedotbin.github.io",
+	vite: {
+		ssr: {
+			noExternal: ["@fontsource/inter"],
+		},
+	},
 });
