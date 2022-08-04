43319e4f9315227e05533cf9d6e93357187986e8
John Carlo Austria <johncarloaustria@protonmail.com>
2022-08-04 21:53:34 +0800

    Add sitemap, add H1 on index page
    Signed-off-by: John Carlo Austria <johncarloaustria@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [image(), tailwind()],
-	site: "https://jaycedotbin.github.io",
-	vite: { ssr: { noExternal: ["@fontsource/inter"] } },
+  integrations: [image(), tailwind(), sitemap()],
+  site: "https://jaycedotbin.github.io",
+  vite: {
+    ssr: {
+      noExternal: ["@fontsource/inter"]
+    }
+  }
 });
