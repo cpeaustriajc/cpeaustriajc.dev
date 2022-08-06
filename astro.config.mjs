bbdcec5704a08b228bc43da1adeb1a68d21cc294
John Carlo Austria <johncarloaustria@protonmail.com>
2022-08-06 14:35:34 +0800

    Remove mdx (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
@@ -18,7 +16,6 @@ export default defineConfig({
 				},
 			},
 		}),
-		mdx(),
 	],
 	site: "https://jaycedotbin.github.io",
 	vite: {
