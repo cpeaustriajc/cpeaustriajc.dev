c39c97af692d643cf31950fbb5232c29eddf6e4f
John Carlo Austria <johncarloaustria@proton.me>
2022-08-14 14:04:36 +0800

    Use svelte
    Signed-off-by: John Carlo Austria <johncarloaustria@proton.me>

--- astro.config.ts
+++ astro.config.ts
@@ -1,33 +1,37 @@
-import react from "@astrojs/react";
 import type { SitemapOptions } from "@astrojs/sitemap";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import type { ViteUserConfig } from "astro/config";
 import { defineConfig } from "astro/config";
 
-const sitemapOptions:SitemapOptions = {
-  customPages: ["https://jaycedotbin.vercel.app/about", "https://jaycedotbin.vercel.app/blog", "https://jaycedotbin.vercel.app/resume", "https://jaycedotbin.vercel.app/uses", "https://jaycedotbin.vercel.app/posts/astro-is-amazing"],
-  i18n: {
-    defaultLocale: "en",
-    locales: {
-      en: "en-US"
-    }
-  }
-}
-
+const sitemapOptions: SitemapOptions = {
+	customPages: [
+		"https://jaycedotbin.vercel.app/about",
+		"https://jaycedotbin.vercel.app/blog",
+		"https://jaycedotbin.vercel.app/resume",
+		"https://jaycedotbin.vercel.app/uses",
+		"https://jaycedotbin.vercel.app/posts/astro-is-amazing",
+	],
+	i18n: {
+		defaultLocale: "en",
+		locales: {
+			en: "en-US",
+		},
+	},
+};
 const viteUserConfig: ViteUserConfig = {
-    ssr: {
-      noExternal: ["@fontsource/inter"]
-    }
-  }
-
+	ssr: {
+		noExternal: ["@fontsource/inter"],
+	},
+};
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  integrations: [tailwind(), sitemap(sitemapOptions),  react()],
-  site: "https://jaycedotbin.github.io",
-  vite: viteUserConfig
+	output: "server",
+	adapter: vercel(),
+	integrations: [tailwind(), sitemap(sitemapOptions), svelte()],
+	site: "https://jaycedotbin.github.io",
+	vite: viteUserConfig,
 });
---
+++ src/components/SpotifyNowPlaying.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	interface Data {
+		albumImageURL: string;
+		songURL: string;
+		title: string;
+		artist: string;
+		album: string;
+		isPlaying: boolean;
+	}
+
+	export let data: Data;
+</script>
+
+<div>
+	<p>Now Playing in Spotify:</p>
+	{#if data.isPlaying}
+		<div>
+			<img src={data.albumImageURL} width="100" height="100" alt={data.album} />
+		</div>
+		<div>
+			<a href={data.songURL}>
+				{data.title} by {data.artist} from the album {data.album}
+			</a>
+		</div>
+	{:else}
+		<p>None</p>
+	{/if}
+</div>
