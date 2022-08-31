ed79b84807e5b532851d1179bf765298894b7def
John Carlo Austria <johncarloaustria@proton.me>
2022-08-31 22:28:32 +0800

    Use svelte for lazy loading videos
    Signed-off-by: John Carlo Austria <johncarloaustria@proton.me>

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import type { ViteUserConfig } from "astro/config";
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
+import svelte from "@astrojs/svelte";
 const viteUserConfig: ViteUserConfig = {
   ssr: {
     noExternal: ["@fontsource/inter"]
@@ -13,6 +14,6 @@ const viteUserConfig: ViteUserConfig = {
 // https://astro.build/config
 export default defineConfig({
   site: "https://jaycedotbin.github.io",
-  integrations: [tailwind(), sitemap(), image()],
+  integrations: [tailwind(), sitemap(), image(), svelte()],
   vite: viteUserConfig
 });
