b8c5070fe295ba09d314d82e2d1728c60f633571
John Carlo Austria <johncarloaustria@proton.me>
2022-09-07 13:23:57 +0800

    Use css for loading fonts
    Signed-off-by: John Carlo Austria <johncarloaustria@proton.me>

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,12 @@
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import type { ViteUserConfig } from "astro/config";
 import { defineConfig } from "astro/config";
 import remarkToc from "remark-toc";
-
-const viteUserConfig: ViteUserConfig = {
-	ssr: {
-		noExternal: ["@fontsource/inter"],
-	},
-};
 
 // https://astro.build/config
 export default defineConfig({
 	markdown: { remarkPlugins: [remarkToc], extendDefaultPlugins: true },
 	site: "https://jaycedotbin.github.io",
 	integrations: [tailwind(), sitemap(), image()],
-	vite: viteUserConfig,
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				sans: ["Inter", "sans-serif"],
+				mono: ["JetBrains Mono", "monospace"],
 			},
 		},
 	},
