79d6c022466d840266b37b39a42bcf03cc99ea01
John Carlo Austria <johncarloaustria@proton.me>
2022-09-08 09:27:38 +0800

    Add reading time
    Signed-off-by: John Carlo Austria <johncarloaustria@proton.me>

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,23 @@ import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import { toString } from "mdast-util-to-string";
+import getReadingTime from "reading-time";
 import remarkToc from "remark-toc";
+
+const remarkReadingTime = () => {
+	// @ts-ignore
+	return (tree, { data }) => {
+		const textOnPage = toString(tree);
+		const readingTime = getReadingTime(textOnPage);
+
+		data.astro.frontmatter.minutesRead = readingTime.text;
+	};
+};
 
 // https://astro.build/config
 export default defineConfig({
-	markdown: { remarkPlugins: [remarkToc], extendDefaultPlugins: true },
+	markdown: { remarkPlugins: [remarkToc, remarkReadingTime], extendDefaultPlugins: true },
 	site: "https://jaycedotbin.github.io",
 	integrations: [tailwind(), sitemap(), image()],
 });
