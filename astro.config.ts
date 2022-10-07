bb07451943a0517c3d027a65afa050b939f1dc79
John Carlo Austria <johncarloaustria@proton.me>
2022-10-07 21:54:52 +0800

    Add resume page
    This commit also adds support
    for tailwindcss, a portrait image

--- astro.config.ts
+++ astro.config.ts
@@ -3,23 +3,25 @@ import { defineConfig } from 'astro/config';
 import { toString } from 'mdast-util-to-string';
 import getReadingTime from 'reading-time';
 import remarkToc from 'remark-toc';
+import tailwind from "@astrojs/tailwind";
 
 const remarkReadingTime = () => {
-	// @ts-ignore
-	return (tree, { data }) => {
-		const textOnPage = toString(tree);
-		const readingTime = getReadingTime(textOnPage);
-
-		data.astro.frontmatter.minutesRead = readingTime.text;
-	};
+  // @ts-ignore
+  return (tree, { data }) => {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    data.astro.frontmatter.minutesRead = readingTime.text;
+  };
 };
+
+
 
 // https://astro.build/config
 export default defineConfig({
-	markdown: {
-		remarkPlugins: [remarkToc, remarkReadingTime],
-		extendDefaultPlugins: true,
-	},
-	site: 'https://jaycedotbin.github.io',
-	integrations: [sitemap()],
+  markdown: {
+    remarkPlugins: [remarkToc, remarkReadingTime],
+    extendDefaultPlugins: true
+  },
+  site: 'https://jaycedotbin.github.io',
+  integrations: [sitemap(), tailwind()]
 });
