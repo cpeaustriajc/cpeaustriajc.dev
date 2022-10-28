2948a4f0d99f099381634013a616bfa436e4b943
John Carlo Austria <johncarloaustria@proton.me>
2022-10-28 09:40:45 +0800

    Add rss

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,11 @@
+import rss from '@astrojs/rss';
+
+export const get = () =>
+	rss({
+		title: 'Blog | John Carlo Austria',
+		description:
+			"John Carlo Austria's Personal Portfolio, A frontend developer based in the Philippines.",
+		site: 'https://jaycedotbin.github.io',
+		items: import.meta.glob('./**/*.md'),
+		customData: `<language>en-us</language>`,
+	});
