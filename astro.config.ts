b0e0b9e6b57c0c98680adbbe02d6fd3f8806a9dc
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2022-12-01 19:09:29 +0800

    Initial pwa support

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 import { toString } from 'mdast-util-to-string';
 import getReadingTime from 'reading-time';
 import remarkToc from 'remark-toc';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import AstroPWA from '@vite-pwa/astro';
 
 const remarkReadingTime = () => {
   // @ts-ignore
@@ -18,8 +19,46 @@ const remarkReadingTime = () => {
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkToc, remarkReadingTime],
-    extendDefaultPlugins: true
+    extendDefaultPlugins: true,
   },
   site: 'https://jaycedotbin.me',
-  integrations: [sitemap(), tailwind()]
+  integrations: [
+    sitemap(),
+    tailwind(),
+    AstroPWA({
+      base: '/',
+      scope: '/',
+      includeAssets: [
+        '/favicon/light/favicon.ico',
+        '/favicon/dark/favicon.ico',
+      ],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'John Carlo Austria | Portfolio',
+        short_name: 'Portfolio',
+        description: "John Carlo Austria's Personal Site",
+        theme_color: '#000000',
+        background_color: '#FFFFFF',
+        start_url: '/',
+        orientation: 'any',
+        icons: [
+          {
+            src: '/favicon/light/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon/light/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+        ],
+        display: 'standalone',
+      },
+      workbox: {
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+    }),
+  ],
 });
