4d20179901f5fb8017400e530063b93c253adb9d
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2022-12-15 21:47:16 +0800

    Add trailing comma

--- astro.config.ts
+++ astro.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
             src: '/favicon/light/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any maskable',
           },
         ],
         display: 'standalone',
