0b4dc32ec933c43bc9b50d5f4408bd485fda57a6
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-01-03 10:00:49 +0800

    Remove pwa support

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import { toString } from 'mdast-util-to-string';
 import getReadingTime from 'reading-time';
 import remarkToc from 'remark-toc';
 import tailwind from '@astrojs/tailwind';
-import AstroPWA from '@vite-pwa/astro';
 
 const remarkReadingTime = () => {
   // @ts-ignore
@@ -25,40 +24,5 @@ export default defineConfig({
   integrations: [
     sitemap(),
     tailwind(),
-    AstroPWA({
-      base: '/',
-      scope: '/',
-      includeAssets: [
-        '/favicon/light/favicon.ico',
-        '/favicon/dark/favicon.ico',
-      ],
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'John Carlo Austria | Portfolio',
-        short_name: 'Portfolio',
-        description: "John Carlo Austria's Personal Site",
-        theme_color: '#000000',
-        background_color: '#FFFFFF',
-        start_url: '/',
-        orientation: 'any',
-        icons: [
-          {
-            src: '/favicon/light/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/favicon/light/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-        display: 'standalone',
-      },
-      workbox: {
-        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
-      },
-    }),
   ],
 });
