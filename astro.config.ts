96b7c967d89bd133f5e6f7342abb0782e8927e3d
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-01-11 21:03:35 +0800

    Format files

--- astro.config.ts
+++ astro.config.ts
@@ -6,23 +6,20 @@ import remarkToc from 'remark-toc';
 import tailwind from '@astrojs/tailwind';
 
 const remarkReadingTime = () => {
-  // @ts-ignore
-  return (tree, { data }) => {
-    const textOnPage = toString(tree);
-    const readingTime = getReadingTime(textOnPage);
-    data.astro.frontmatter.minutesRead = readingTime.text;
-  };
+	// @ts-ignore
+	return (tree, { data }) => {
+		const textOnPage = toString(tree);
+		const readingTime = getReadingTime(textOnPage);
+		data.astro.frontmatter.minutesRead = readingTime.text;
+	};
 };
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [remarkToc, remarkReadingTime],
-    extendDefaultPlugins: true,
-  },
-  site: 'https://jaycedotbin.me',
-  integrations: [
-    sitemap(),
-    tailwind(),
-  ],
+	markdown: {
+		remarkPlugins: [remarkToc, remarkReadingTime],
+		extendDefaultPlugins: true,
+	},
+	site: 'https://jaycedotbin.me',
+	integrations: [sitemap(), tailwind()],
 });
