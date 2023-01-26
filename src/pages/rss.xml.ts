6020622d3e6bdd60206ab165c4797bd74eef686a
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-01-26 17:39:47 +0800

    Use content collections for getting data

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,19 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
-export const get = () =>
-	rss({
+export async function get() {
+	const blog = await getCollection('blog');
+	return rss({
 		title: 'Blog | John Carlo Austria',
 		description:
 			"John Carlo Austria's Personal Portfolio, A frontend developer based in the Philippines.",
 		site: 'https://jaycedotbin.me',
-		items: import.meta.glob('./**/*.md'),
+		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			link: `/blog/${post.slug}`,
+		})),
 		customData: `<language>en-us</language>`,
 	});
+}
