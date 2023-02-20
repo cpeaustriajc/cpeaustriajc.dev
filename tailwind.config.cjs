641da740b78f361e8c92611280067f765378c617
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-02-20 10:51:18 +0800

    Use roboto mono for the monospace font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,16 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				display: ['OswaldVariable', ...defaultTheme.fontFamily.sans],
-				sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [require('@tailwindcss/typography')],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["OswaldVariable", "Oswald", ...defaultTheme.fontFamily.sans],
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["Roboto Mono", ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
