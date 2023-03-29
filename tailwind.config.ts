2efd4429c327f7ef994ac7bb53aa8003d8f570cc
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-03-29 11:46:24 +0800

    Remove jsdoc type import comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 import type { Config } from "tailwindcss";
 
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
