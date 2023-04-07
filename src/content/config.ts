0aafdb9f7e72548b21dcfabf729e7080353453cf
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-04-07 10:31:54 +0800

    Remove image import in astro:content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,15 @@
-import { z, defineCollection, image } from "astro:content";
+import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    publishedDate: z.date(),
-    topics: z.array(z.string()),
-    cover: image(),
-    draft: z.boolean(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      publishedDate: z.date(),
+      topics: z.array(z.string()),
+      cover: image(),
+      draft: z.boolean(),
+    }),
 });
 
 export const collections = {
