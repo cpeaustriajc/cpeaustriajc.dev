83388ada6b62594b865671ad9abcc492ca39c66f
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-05-20 18:45:36 +0800

    Dark mode, max width 65ch

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="dark:bg-slate-950 dark:text-slate-50">
+        <main className="max-w-prose">{children}</main>
+      </body>
     </html>
   );
 }
