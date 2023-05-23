bc49ec010b7e4581344ab491ba2c8ef05d3284ca
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-05-23 09:58:01 +0800

    Add resume and studio routes

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,8 +12,19 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <NavigationMenu>
+    <NavigationMenu className="my-2">
       <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="https://read.cv/jaycedotbin" legacyBehavior passHref>
+            <NavigationMenuLink
+              rel="noopener noreferrer"
+              target="_blank"
+              className={navigationMenuTriggerStyle()}
+            >
+              Resume
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
         <NavigationMenuItem>
           <Link href="/" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
@@ -25,6 +36,13 @@ export default function Header() {
           <Link href="/blog" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Blog
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/studio" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Studio
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
