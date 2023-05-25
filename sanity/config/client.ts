ae080bc98f8384ec37d0c702d46140272ccd213c
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-05-25 22:20:55 +0800

    set useCdn to false

--- sanity/config/client.ts
+++ sanity/config/client.ts
@@ -4,6 +4,7 @@ const config = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: '2023-03-09',
+  useCdn: false,
 } satisfies ClientConfig;
 
 export default config;
