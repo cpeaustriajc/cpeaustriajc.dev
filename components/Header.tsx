64dae2fa4b4c796ca486112ab2867713eebe8ebd
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-05-26 15:27:27 +0800

    Revert "Show studio link if development or preview"
    This reverts commit bdc17185809ae3cabfffa8dec06e9689754cb808.

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,13 +40,15 @@ export default function Header() {
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/studio" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Studio
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+          {process.env.VERCEL_ENV === 'development' || 'preview' && (
+            <NavigationMenuItem>
+              <Link href="/studio" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Studio
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          )}
         </NavigationMenuList>
       </NavigationMenu>
     </header>
