7f07ef987a0c66e65d86c8edbe36fa2f9e35a27a
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-05-27 21:12:11 +0800

    Move portable text components to components folder

--- app/blog/post/[slug]/page.tsx
+++ app/blog/post/[slug]/page.tsx
@@ -1,77 +1,11 @@
 import { getPost, urlFor } from '@/sanity/utils';
 import { Metadata } from 'next';
-import { PortableText, PortableTextComponents } from '@portabletext/react';
-import Code from '@/components/Code';
-import Link from 'next/link';
+import { PortableText } from '@portabletext/react';
+import { components } from '@/components/ui/portable-text-components';
 
 type Props = {
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
-};
-
-const components: PortableTextComponents = {
-  block: {
-    h1: ({ children }) => (
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        {children}
-      </h1>
-    ),
-    h2: ({ children }) => (
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-        {children}
-      </h2>
-    ),
-    h3: ({ children }) => (
-      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-        {children}
-      </h3>
-    ),
-    h4: ({ children }) => (
-      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-        {children}
-      </h4>
-    ),
-    normal: ({ children }) => (
-      <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>
-    ),
-    blockquote: ({ children }) => (
-      <blockquote className="mt-6 border-l-2 pl-6 italic">
-        {children}
-      </blockquote>
-    ),
-  },
-  marks: {
-    'strike-through': ({ children }) => (
-      <del className="line-through text-gray-500">{children}</del>
-    ),
-    strong: ({ children }) => (
-      <strong className="font-semibold">{children}</strong>
-    ),
-    em: ({ children }) => <em className="italic">{children}</em>,
-    link: ({ value, children }) => {
-      return (
-        <Link
-          className="ont-medium text-primary underline underline-offset-4"
-          href={value?.href}
-        >
-          {children}
-        </Link>
-      );
-    },
-  },
-  types: {
-    code: ({ value }) => (
-      <Code language={value.language || 'javascript'} code={value.code} />
-    ),
-  },
-  list: {
-    bullet: ({ children }) => (
-      <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>
-    ),
-    number: ({ children }) => (
-      <ol className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ol>
-    ),
-  },
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
