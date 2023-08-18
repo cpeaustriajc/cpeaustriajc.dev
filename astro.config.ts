2af097f5f4359a89e0ae01df10acc796412e195a
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-08-18 10:44:38 +0800

    Remove svelte, use web components

--- astro.config.ts
+++ astro.config.ts
@@ -2,14 +2,12 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true,
     viewTransitions: true,
   },
-  integrations: [sitemap(), tailwind(), svelte()],
+  integrations: [sitemap(), tailwind()],
   site: "https://jaycedotbin.me",
 });
