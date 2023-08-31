01abea5c205abf55a7ce6a91d548ccac08d53539
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-08-31 09:56:41 +0800

    Specify that the deploy target is vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,15 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    analytics: true,
+    imageService: true,
+  }),
   integrations: [sitemap(), tailwind()],
   site: "https://jaycedotbin.me",
 });
