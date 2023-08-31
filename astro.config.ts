07a3eb45b717c322f0f8dd738316313af0bbd5da
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-08-31 11:54:22 +0800

    Add mdx support

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel({
     analytics: true,
-    imageService: true,
+    imageService: true
   }),
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), mdx()],
   site: "https://jaycedotbin.me",
 });
