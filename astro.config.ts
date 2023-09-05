e10d529d18bc385b7570458d0f797e841b534c7b
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-09-05 11:03:02 +0800

    Use hybrid rendering

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,13 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "hybrid",
   adapter: vercel({
     analytics: true,
     imageService: true
