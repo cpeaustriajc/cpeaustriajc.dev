178bf2800a56d73375dd22545613022193d21792
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-09-10 21:26:25 +0800

    Add more content collections schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,49 @@
 import { z, defineCollection } from "astro:content";
 
 const postCollection = defineCollection({
+  type: "content",
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string(),
       publishDate: z.string().transform((str) => new Date(str)),
       keywords: z.array(z.string()),
-      coverImage: image().refine((img) => img.width >= 1200, {
-        message: "Cover Image must be at least 1200 pixels wide!",
-      }),
+      coverImage: image()
+        .refine((img) => img.width >= 360, {
+          message: "Cover Image must be at least 1200 pixels wide!",
+        }),
       coverImageAlt: z.string(),
       draft: z.boolean(),
     }),
 });
 
+const projectsCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      link: z.string().url(),
+      coverImage: image(),
+      coverImageAlt: z.string(),
+    }),
+});
+
+const skillsCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      link: z.string().url(),
+      coverImage: image()
+        .refine((img) => img.width >= 360, {
+          message: "Cover Image must be at least 360 pixels wide!",
+        }),
+      coverImageAlt: z.string(),
+    }),
+});
+
 export const collections = {
   posts: postCollection,
+  projects: projectsCollection,
+  skills: skillsCollection,
 };
