d90e60243128da465122901d3d69df35c5e6d176
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-09-10 21:54:38 +0800

    Make the scroll-timeline a tailwind plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 import typography from "@tailwindcss/typography";
 import colors from "tailwindcss/colors";
+import plugin from "tailwindcss/plugin";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -21,30 +22,57 @@ export default {
       animation: {
         "gradient-x": "gradient-x 4s ease infinite",
         "fade-in": "fade-in 0.5s ease-out forwards",
+        "scroll-fade": "scroll-fade 0.5s ease-out forwards",
       },
-    },
-    keyframes: {
-      "gradient-x": {
-        "0%, 100%": {
-          "background-size": "200% 200%",
-          "background-position": "left center",
+      keyframes: {
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
         },
-        "50%": {
-          "background-size": "200% 200%",
-          "background-position": "right center",
-        },
-      },
-      "fade-in": {
-        "0%": {
-          opacity: "0",
-          transform: "translateY(4rem)",
-        },
-        "100%": {
-          opacity: "1",
-          transform: "translateY(0)",
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(4rem)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
         },
       },
     },
   },
-  plugins: [typography()],
+  plugins: [
+    typography(),
+    plugin(({ addBase, addUtilities }) => {
+      addBase({
+        "@keyframes scroll-fade": {
+          "0%": {
+            opacity: "0",
+          },
+
+          "100%": {
+            opacity: "1",
+          },
+        },
+      });
+
+      addUtilities({
+        ".in-view": {
+          "animation-timeline": "view(block 25% 25%)",
+          "animation-name": "scroll-fade",
+          "animation-fill-mode": "both",
+          "animation-timing-function": "ease-in-out",
+          "transform-origin": "top",
+          "animation-duration": "500ms",
+        },
+      });
+    }),
+  ],
 } satisfies Config;
