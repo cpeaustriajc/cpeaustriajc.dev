82076afa1bd087e4527c734797224b939af85042
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-14 16:42:12 +0800

    Remove webAnalytics

--- astro.config.ts
+++ astro.config.ts
@@ -2,21 +2,17 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    imageService: true,
+    imageService: true
   }),
   image: {
-    domains: ["avatars.githubusercontent.com"],
+    domains: ["avatars.githubusercontent.com"]
   },
   integrations: [sitemap(), tailwind(), mdx()],
-  site: "https://jayzers.vercel.app",
+  site: "https://jayzers.vercel.app"
 });
