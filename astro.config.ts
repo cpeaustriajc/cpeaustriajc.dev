c8ef83e3d440cb6ffb56cff1e1e7fa5065d77c41
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-26 13:46:20 +0800

    Purge tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,13 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: vercel({
-    imageService: true,
-  }),
   image: {
     domains: ["avatars.githubusercontent.com"],
   },
-  integrations: [sitemap(), tailwind(), mdx()],
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: { wrap: true, theme: "vitesse-black" },
   },
