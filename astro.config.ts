4ac002acdce2fbfb4d286a0266c5c657bbfd3de9
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2024-08-10 22:15:19 +0800

    Remove svelte

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,11 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["avatars.githubusercontent.com", "https://emojicdn.elk.sh/"],
   },
-  integrations: [sitemap(), icon(), svelte()],
+  integrations: [sitemap(), icon()],
   site: "https://cpeaustriajc.github.io",
 });
