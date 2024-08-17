309b7df236d8b63f4a606155dd688a86d5f5ae1b
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2024-08-17 22:02:53 +0800

    Improve checking of title and description for SEO

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,32 @@
 import { defineCollection, reference, z } from "astro:content";
 
+function refineTitle() {
+  return {
+    check: (title: string) => title.length >= 50 || title.length <= 60,
+    message: "Title must be between 50 and 60 characters!",
+  };
+}
+
+function refineDescription() {
+  return {
+    check: (desc: string) => desc.length >= 50 || desc.length <= 160,
+    message: "Description must be between 50 and 160 characters!",
+  };
+}
+
 const postCollection = defineCollection({
   type: "content",
   schema: ({ image }) =>
     z.object({
-      title: z.string(),
-      description: z.string(),
+      title: z.string().refine(refineTitle),
+      description: z.string().refine(refineDescription),
       publishDate: z.date(),
       keywords: z.array(z.string()),
       author: reference("authors"),
-      coverImage: image().refine((img) => img.width >= 360, {
+      coverImage: image().refine((img) => img.width >= 1200, {
         message: "Cover Image must be at least 1200 pixels wide!",
       }),
-      coverImageAlt: z.string(),
+      coverImageAlt: z.string().refine(refineDescription),
       draft: z.boolean(),
     }),
 });
@@ -21,9 +35,9 @@ const projectsCollection = defineCollection({
   type: "data",
   schema: () =>
     z.object({
-      title: z.string(),
+      title: z.string().refine(refineTitle),
       link: z.string().url(),
-      description: z.string(),
+      description: z.string().refine(refineDescription),
       status: z.enum(["active", "archived"]),
       site: z.string().url(),
     }),
@@ -42,10 +56,10 @@ const metaCollection = defineCollection({
   type: "data",
   schema: ({ image }) =>
     z.object({
-      title: z.string(),
-      description: z.string(),
+      title: z.string().refine(refineTitle),
+      description: z.string().refine(refineDescription),
       image: image(),
-      imageAlt: z.string(),
+      imageAlt: z.string().refine(refineDescription),
       keywords: z.array(z.string()),
     }),
 });
